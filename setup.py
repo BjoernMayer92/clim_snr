157e0b65bc66a6093cd301fc75f4e2d44ee20506
Björn Mayer <bjoern.mayer92@gmail.com>
2021-09-06 11:45:32 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='clim_snr',
+      version='0.1',
+      description='Calculation of signal to noise ratios in ensemble forecasts of climate data',
+      #url='http://github.com/storborg/funniest',
+      author='Bjoern Mayer',
+      author_email='bjoern.mayer92@gmail.com',
+      license='MIT',
+      packages=['clim_snr'],
+      zip_safe=False)
